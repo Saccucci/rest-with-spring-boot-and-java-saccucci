e9530a7c3064764e6903f9f6817127f371107e4f
Pedro Saccucci <pedro.saccucci@gmail.com>
2025-09-24 00:07:51 -0300

    Implementando a Entidade Person e o Repositório

--- 07_SpringDataJPAWithMySQL/src/main/java/br/com/saccucci/model/Person.java
+++ 07_SpringDataJPAWithMySQL/src/main/java/br/com/saccucci/model/Person.java
@@ -2,14 +2,33 @@ package br.com.saccucci.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person") // Só preciso passar o name = "nome" quando mudar a entidade e/ou variável
 public class Person implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
     
     public Person() {
